49d9a58b6de5dfdab145f2761b9bc4727fb5610b
zbs <zhangbaoshuai@cph.citic>
2022-12-22 16:16:48 +0800

    feat(deps): eureka【provide、consumer、api】三方通过mybatisplus连接数据库调用

---
+++ spring-cloud-eureka-consumer/src/main/java/com/zbs/springcloudeurekaconsumer/controller/DeptController.java
@@ -0,0 +1,50 @@
+package com.zbs.springcloudeurekaconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * description: EmployeeController
+ * date: 2021/8/12 23:41
+ * author: zbs
+ * version: 1.0
+ */
+@RestController
+@RequestMapping("/dept")
+public class DeptController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /***
+     * 注入discoveryClient 注意是Spring的包
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/queryList")
+    public String getTest() {
+        //获取实例列表
+        List<ServiceInstance> instancesList = discoveryClient.getInstances("emp-provider");
+        //获取实例
+        ServiceInstance instance = instancesList.get(0);
+        //获取主机地址
+        String hostName = instance.getHost();
+        //获取端口号
+        int port = instance.getPort();
+        //拼接url
+        String url = "http://" + hostName + ":" + port + "/dept/queryList";
+        //调用接口
+        String result = restTemplate.getForObject(url, String.class);
+        //返回结果
+        return result;
+    }
+
+}
--- spring-cloud-eureka-provider/src/main/java/com/zbs/springcloudeurekaprovider/SpringCloudEurekaProviderApplication.java
+++ spring-cloud-eureka-provider/src/main/java/com/zbs/springcloudeurekaprovider/SpringCloudEurekaProviderApplication.java
@@ -1,11 +1,13 @@
 package com.zbs.springcloudeurekaprovider;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.zbs.springcloudeurekaprovider.db.mapper")
 public class SpringCloudEurekaProviderApplication {
 
     public static void main(String[] args) {
