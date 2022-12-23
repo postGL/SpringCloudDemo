8972f43a694ba8bd7b6193c2c32f5dd522d6397f
zbs <zhangbaoshuai@cph.citic>
2022-12-23 11:06:56 +0800

    feat(deps): @EnableDiscoveryClient(适用于eureka、nacos等多个注册中心)>=@EnableEurekaClient(仅适用于eureka注册中心，后边版本被抛弃)

--- spring-cloud-eureka-consumer/src/main/java/com/zbs/springcloudeurekaconsumer/SpringCloudEurekaConsumerApplication.java
+++ spring-cloud-eureka-consumer/src/main/java/com/zbs/springcloudeurekaconsumer/SpringCloudEurekaConsumerApplication.java
@@ -4,7 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @EnableEurekaClient只适用于使用Eureka作为注册中心的场景，@EnableDiscoveryClient可以适用于其他注册中心的场景比如nacos等。
+ *
+ * 在Spring Cloud的Dalston及其之前的版本中：
+ *  1、从2014年的Spring Cloud 1.0.0.RC1版本开始，官方就推荐使用EnableDiscoveryClient来取代EnableEurekaClient。
+ *  2、EnableEurekaClient源码中使用了注解EnableDiscoveryClient，因此如果要使用eureka的注册发现服务，两者功能是一样的。
+ *  3、EnableDiscoveryClient注解在spring.factories配置中通过配置项EurekaDiscoveryClientConfiguration来开启服务注册发现功能；
+ *
+ * 在Dalston之后的版本中（不含Dalston）：
+ *  1、在spring.factories配置中，配置类EurekaDiscoveryClientConfiguration被配置到springboot的自动配置注解中，与EnableDiscoveryClient注解没有关系了,也就是说只要开启了springboot的自动配置，服务注册发现功能就会启用。
+ *  2、 EnableEurekaClient源码中没有使用注解EnableDiscoveryClient，此时EnableEurekaClient已经没用了。
+ *
+ */
 @EnableDiscoveryClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudEurekaConsumerApplication {
 
