c99efa7c640787520eb6b27a725db2bc8be501b4
zbs <zhangbaoshuai@cph.citic>
2022-12-23 23:25:24 +0800

    feat(deps): 熔断方式二：FeignClient fallback方式和fallbackFactory方式

--- spring-cloud-eureka-api/src/main/java/com/zbs/eureka/api/service/DeptFeignService.java
+++ spring-cloud-eureka-api/src/main/java/com/zbs/eureka/api/service/DeptFeignService.java
@@ -4,8 +4,7 @@ import com.zbs.eureka.api.beans.DeptDto;
 import com.zbs.eureka.api.service.impl.DeptFeignServiceFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * description: DeptFeginService
@@ -13,10 +12,17 @@ import java.util.List;
  * author: zhangbs
  * version: 1.0
  */
+
+/**
+ * FeignClient 除了fallback方式，还有fallbackFactory方式
+ * 一、fallbackFactory 推荐：可以捕获异常信息并返回默认降级结果。可以打印堆栈信息。
+ * 二、 fallback 不推荐:不能捕获异常打印堆栈信息，不利于问题排查。
+ * https://blog.csdn.net/weixin_42771651/article/details/121626431
+ */
 @FeignClient(value = "EMP-PROVIDER", fallback = DeptFeignServiceFallBack.class)
 public interface DeptFeignService {
 
-    @GetMapping("/dept/queryList")
-    List<DeptDto> queryDeptList();
+    @GetMapping("/dept/queryById/{id}")
+    DeptDto queryById(@PathVariable int id);
 
 }
--- spring-cloud-eureka-api/src/main/java/com/zbs/eureka/api/service/impl/DeptFeignServiceFallBack.java
+++ spring-cloud-eureka-api/src/main/java/com/zbs/eureka/api/service/impl/DeptFeignServiceFallBack.java
@@ -4,9 +4,6 @@ import com.zbs.eureka.api.beans.DeptDto;
 import com.zbs.eureka.api.service.DeptFeignService;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * description: DeptFeignServiceFallBack
  * date: 2022/12/23 18:41
@@ -17,13 +14,11 @@ import java.util.List;
 public class DeptFeignServiceFallBack implements DeptFeignService {
 
     @Override
-    public List<DeptDto> queryDeptList() {
-        List<DeptDto> deptDtoList = new ArrayList<>();
-        log.error("未查询到数据");
+    public DeptDto queryById(int id) {
+        log.error("未查询到数据！！！");
         DeptDto deptDto = new DeptDto()
                 .setDeptno(-1L)
                 .setDname("未找到该部门~");
-        deptDtoList.add(deptDto);
-        return deptDtoList;
+        return deptDto;
     }
 }
