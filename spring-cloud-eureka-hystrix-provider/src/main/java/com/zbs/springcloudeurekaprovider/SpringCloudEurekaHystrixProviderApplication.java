f7496fae99ab5d0f3b6910cdf564eb2b619054f3
zbs <zhangbaoshuai@cph.citic>
2022-12-25 18:45:29 +0800

    refactor(deps): 修改注释格式，否则格式化，会格式掉注释

--- spring-cloud-eureka-hystrix-provider/src/main/java/com/zbs/springcloudeurekaprovider/SpringCloudEurekaHystrixProviderApplication.java
+++ spring-cloud-eureka-hystrix-provider/src/main/java/com/zbs/springcloudeurekaprovider/SpringCloudEurekaHystrixProviderApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @Mapper 是对单个接口类的注解。单个操作。
- * @MapperScan 是对整个包下的所有的接口类的注解。是批量的操作。使用 @MapperScan 后，接口类 就不需要使用 @Mapper 注解。
- * @MapperScan：支持扫描多个包，扫描子包
- */
+// @Mapper 是对单个接口类的注解。单个操作。
+// @MapperScan 是对整个包下的所有的接口类的注解。是批量的操作。使用 @MapperScan 后，接口类 就不需要使用 @Mapper 注解。
+// @MapperScan：支持扫描多个包，扫描子包
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.zbs.springcloudeurekaprovider.db.mapper")
