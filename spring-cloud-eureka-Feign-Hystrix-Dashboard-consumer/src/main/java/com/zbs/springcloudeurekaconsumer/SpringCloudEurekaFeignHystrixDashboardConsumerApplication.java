d82281cb5d74e03b54a23c3f4bba4b4b8d3b86cf
zbs <zhangbaoshuai@cph.citic>
2022-12-25 19:02:43 +0800

    feat(deps): Dashboard监控面板操作步骤

--- spring-cloud-eureka-Feign-Hystrix-Dashboard-consumer/src/main/java/com/zbs/springcloudeurekaconsumer/SpringCloudEurekaFeignHystrixDashboardConsumerApplication.java
+++ spring-cloud-eureka-Feign-Hystrix-Dashboard-consumer/src/main/java/com/zbs/springcloudeurekaconsumer/SpringCloudEurekaFeignHystrixDashboardConsumerApplication.java
@@ -6,13 +6,18 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 
 /**
  * 监控页面项目：豪猪
- * @EnableHystrixDashboard // 客户端开启Hystrix Dashboard监控
- * 服务端也必须有spring-boot-starter-actuator监控
+ * 开启@EnableHystrixDashboard // 客户端开启Hystrix Dashboard监控
+ * （1）客户端配置文件：
+ * hystrix:
+ *   dashboard:
+ *     proxy-stream-allow-list: "localhost"
+ * （2）服务端：必须有spring-boot-starter-actuator监控
  * 		<dependency>
  * 			<groupId>org.springframework.boot</groupId>
  * 			<artifactId>spring-boot-starter-actuator</artifactId>
  * 		</dependency>
- * 	监控访问地址：http://localhost:8092/hystrix
+ * 	监控访问地址：http://localhost:8092/hystrix（备注：需要配置文件）
+ * 	服务端地址：http://localhost:9000/actuator/hystrix.stream（备注：1、需要添加actuator pom 2、注册HystrixMetricsStreamServlet 3、开启Hystrix @EnableCircuitBreaker）
  */
 @SpringBootApplication
 @EnableHystrixDashboard // 客户端开启Hystrix Dashboard监控
